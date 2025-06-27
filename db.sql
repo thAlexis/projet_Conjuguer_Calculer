6c0891639b033d843a21c7373fd3ac766ac403ab
Alexis Thullier <thullier.alexis@hotmail.fr>
2025-06-27 21:50:59 +0200

    Adding SQL script for db and tables

---
+++ db.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS conjuguer_calc_project;
+
+CREATE DATABASE conjuguer_calc_project;
+
+USE conjuguer_calc_project;
+
+CREATE TABLE utilisateurs (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(30) UNIQUE,
+  password VARCHAR(256),
+  nom VARCHAR(30)
+)
+
+CREATE TABLE histo_conjug (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  userId INT,
+  verbe VARCHAR(30),
+  temps VARCHAR(30),
+  FOREIGN KEY (userId) REFERENCES utilisateurs(id)
+)
+
+CREATE TABLE histo_calc (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  userId INT,
+  valeur1 INT,
+  operateur VARCHAR(1),
+  valeur2 INT,
+  FOREIGN KEY (userId) REFERENCES utilisateurs(id)
+)
